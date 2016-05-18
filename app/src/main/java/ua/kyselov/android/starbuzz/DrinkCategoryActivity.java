5d605176b9c7db2c69256a5a8bfe7b6feabd581a
Ivan Kyselov <kyselovivan@gmail.com>
2016-05-18 12:04:14 +0300

    add DrinkCategoryActivity

---
+++ app/src/main/java/ua/kyselov/android/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,18 @@
+package ua.kyselov.android.starbuzz;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listDrinks = getListView();
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(this,
+                android.R.layout.simple_list_item_1, Drink.drinks);
+        listDrinks.setAdapter(listAdapter);
+    }
+}
--- app/src/main/java/ua/kyselov/android/starbuzz/TopLevelActivity.java
+++ app/src/main/java/ua/kyselov/android/starbuzz/TopLevelActivity.java
@@ -1,13 +1,32 @@
 package ua.kyselov.android.starbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        listView = (ListView)findViewById(R.id.list_options);
+
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        if(position == 0){
+                            Intent intent = new Intent(TopLevelActivity.this,DrinkCategoryActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                };
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
