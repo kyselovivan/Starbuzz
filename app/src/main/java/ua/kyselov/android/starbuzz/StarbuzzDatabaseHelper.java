1271f03d53ae9394a41676eeecd68463435e2501
Ivan Kyselov <kyselovivan@gmail.com>
2016-05-29 00:50:30 +0300

    StarbuzzDatabaseHelper added

---
+++ app/src/main/java/ua/kyselov/android/starbuzz/StarbuzzDatabaseHelper.java
@@ -0,0 +1,28 @@
+package ua.kyselov.android.starbuzz;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Ivan on 5/29/2016.
+ */
+public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "starbuzz" ;
+    private static final int DB_VERSION = 1;
+
+    StarbuzzDatabaseHelper(Context context){
+        super(context,DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
