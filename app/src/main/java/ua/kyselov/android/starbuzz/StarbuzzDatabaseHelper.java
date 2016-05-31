8ec39a3a3367217d0ab5c0bd1a2bd314770c3932
Ivan Kyselov <kyselovivan@gmail.com>
2016-05-31 23:24:24 +0300

    onUpdateMyDatabase() added

--- app/src/main/java/ua/kyselov/android/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/ua/kyselov/android/starbuzz/StarbuzzDatabaseHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "starbuzz" ;
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
 
     StarbuzzDatabaseHelper(Context context){
         super(context,DB_NAME, null, DB_VERSION);
@@ -19,13 +19,32 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE DRINK("
+        updateMyDatabase(db, 0, DB_VERSION);
+//
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if(oldVersion<1){
+            db.execSQL("CREATE TABLE DRINK("
                         + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                         + "NAME TEXT,"
                         + "DESCRIPTION TEXT,"
                         + "IMAGE_RESOURCE_ID INTEGER);"
         );
+        insertDrink(db, "Latte", "A couple of espresso shots with steamed milk", R.drawable.latte);
+        insertDrink(db,"Cappuccino","Espresso, hot milk, and steamed milk foam",R.drawable.cappuccino);
+        insertDrink(db, "Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter);
+        }
+        if(oldVersion<2){
+            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC");
+        }
     }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
 
     public static void insertDrink(SQLiteDatabase db, String name, String description, int resourceId){
         ContentValues drinkValues = new ContentValues();
@@ -33,10 +52,5 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
         drinkValues.put("DESCRIPTION", description);
         drinkValues.put("IMAGE_RESOURCE_ID",resourceId);
         db.insert("DRINK",null,drinkValues);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
     }
 }
